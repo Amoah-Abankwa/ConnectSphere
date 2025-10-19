be029addd37eca0cb3f97307f68b906dcc01251e
Amoah-Abankwa <emmanuelohene11@gmail.com>
2025-10-19 04:04:49 +0000

    Refactor EasySharing component layout and styles

--- frontend/app/components/HomeScreen/EasySharing.tsx
+++ frontend/app/components/HomeScreen/EasySharing.tsx
@@ -1,14 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
-import { CustomText, CustomTextBold, CustomTextMedium, CustomTextSemiBold } from '../ui/CustomText';
+import { CustomText, CustomTextBold, CustomTextMedium, CustomTextSemiBold } from '../UI/CustomText';
 import EasyShareIcon from '../../assets/icons/General/share.svg';
 
 const EasySharing = () => {
   return (
     <View style={styles.container}>
-        <EasyShareIcon style={styles.icon} />
-        <CustomTextBold>Easy Sharing</CustomTextBold>
-        <CustomText>Share your profile via link or QR Code instantly.</CustomText>
+       <View style={styles.iconContainer}>
+          <EasyShareIcon width={24} height={24} />
+        </View>
+        <CustomTextBold style={styles.contentTitle}>Easy Sharing</CustomTextBold>
+        <CustomText style={styles.contentSubtitle}>Share your profile via link or QR Code instantly.</CustomText>
     </View>
   )
 }
@@ -19,7 +21,6 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
         padding: 20,
-        marginHorizontal: 20,
         paddingVertical: 10,
         marginBottom: 10,
         minHeight: 10,
@@ -28,12 +29,24 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         flexDirection: 'column',
     }, 
-     icon: {
-      height: 20,
-      width: 20,
-      marginBottom: 10,
-      borderRadius: 20,
-      backgroundColor: '#fbd3d3ff',
-      padding: 8
-    }
+    
+    
+    iconContainer: {
+    backgroundColor: '#b4d4f6ff', // Blue circle background
+    borderRadius: 25,
+    padding: 10,
+    marginBottom: 10,
+  },
+  contentTitle: {
+    marginBottom: 5,
+    fontSize: 18,
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#000',
+  },
+  contentSubtitle: {
+    textAlign: 'center',
+    fontSize: 14,
+    color: '#555',  
+  }
 })
